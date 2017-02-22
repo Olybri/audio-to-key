7c427896663765e8ab3f654ed1442245431fb6d5
Loris Witschard <loris.wit@gmail.com>
2017-02-22 14:38:43 +0100

    Add parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,120 @@
 #include <SFML/Audio/SoundBufferRecorder.hpp>
 #include <SFML/System/Clock.hpp>
 #include <windows.h>
+#include <csignal>
 #include <iostream>
+#include <limits>
+
+volatile sig_atomic_t flag = 0;
+void handleSig(int sig)
+{
+    flag = 1;
+}
 
 class MyRecorder : public sf::SoundRecorder
 {
 public:
-    MyRecorder()
+    MyRecorder(short threshold, int release, int interval)
+    : m_amplitude {0}, m_decrease{0}
     {
-        setProcessingInterval(sf::milliseconds(10));
+        m_threshold = threshold;
+        m_release = release;
+        m_interval = interval;
+
+        setProcessingInterval(sf::milliseconds(m_interval));
+
+        int amp = m_threshold;
+        amp *= m_screenWidth;
+        amp /= 32800;
+
+        std::cout << "    ";
+
+        for(int i=0; i<amp; ++i)
+            std::cout << " ";
+
+        std::cout << "Threshold\n    ";
+
+        for(int i=0; i<m_screenWidth; ++i)
+            std::cout << (i == amp ? "T" : "-");
+
+        std::cout << std::endl;
     }
 
 private:
+    short m_threshold;
+    short m_amplitude;
+    int m_interval;
+    int m_release;
+    short m_decrease;
+
+    bool m_pressed = false;
+
+    sf::Clock clock;
+
+    const int m_screenWidth = 80;
+
     bool onProcessSamples(const sf::Int16* samples, std::size_t sampleCount) override
     {
-        std::vector<short int> buffer(samples, samples + sampleCount);
+        if(flag)
+            return false;
 
-        int max = 0;
+        if(m_amplitude - m_decrease < 0)
+            m_amplitude = 0;
+        else
+            m_amplitude -= m_decrease;
+
+        std::vector<short> buffer(samples, samples + sampleCount);
+
         for(auto sample : buffer)
-            if(std::abs(sample) > max)
-                max = std::abs(sample);
+            if(std::abs(sample) > m_amplitude)
+            {
+                m_amplitude = std::abs(sample);
+                m_decrease = m_amplitude / (m_release / m_interval);
+            }
 
-        if(max > 8000)
+        INPUT ip;
+        ip.type = INPUT_KEYBOARD;
+        ip.ki.wScan = 0;
+        ip.ki.time = 0;
+        ip.ki.dwExtraInfo = 0;
+        ip.ki.wVk = VK_CONTROL;
+
+        if(m_amplitude > m_threshold)
         {
-            keybd_event(0x47, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
-            keybd_event(0x47, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+            if(!m_pressed)
+            {
+                m_pressed = true;
+
+                ip.ki.dwFlags = 0;
+                SendInput(1, &ip, sizeof(INPUT));
+            }
+
+            std::cout << "ON  ";
         }
+        else
+        {
+            if(m_pressed)
+            {
+                m_pressed = false;
+
+                ip.ki.dwFlags = KEYEVENTF_KEYUP;
+                SendInput(1, &ip, sizeof(INPUT));
+            }
+
+            std::cout << "OFF ";
+        }
+
+        int amp = m_amplitude;
+        amp *= m_screenWidth;
+        amp /= 32800;
+
+        for(int i=0; i<amp; ++i)
+            std::cout << "O";
+
+        for(int i=0; i<m_screenWidth - amp; ++i)
+            std::cout << "-";
+
+        std::cout << "\r" << std::flush;
 
         return true;
     }
@@ -34,15 +123,22 @@ private:
 int main()
 {
     std::cout.sync_with_stdio(false);
+
+    std::cout << "Starting Audio-to-Key..." << std::endl;
+
     auto availableDevices = sf::SoundRecorder::getAvailableDevices();
+    std::cout << "Available devices:\n";
+    for(unsigned i=0; i<availableDevices.size(); ++i)
+        std::cout << i+1 << ". " << availableDevices[i] << std::endl;
 
-    for(auto device : availableDevices)
-        std::cout << device << std::endl;
+    std::cout << "Press Ctrl+C to exit." << std::endl;
 
-    MyRecorder rec;
-    sf::Clock clock;
+    MyRecorder rec(8000, 500, 50);
     rec.start();
 
-    std::cin.get();
+    signal(SIGINT, handleSig);
+    while(!flag);
+    std::cout << "\nExiting..." << std::endl;
+
     rec.stop();
 }
